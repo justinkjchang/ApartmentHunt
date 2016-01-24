846be05edd767e37146cc5448921f98c0f5cbfc1
justinkjchang <justin.kj.chang@gmail.com>
2016-01-24 04:49:20 -0500

    Fixed small error

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,7 @@ var app = angular.module('myApp', [
   'myApp.controllers',
   'ngCsvImport'
 ]).
-app.config(['$routeProvider', function($routeProvider) {
+config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
