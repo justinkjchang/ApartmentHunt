673f129186966d3ac26225589d7fb673c122d86c
justinkjchang <justin.kj.chang@gmail.com>
2016-02-03 04:45:29 -0500

    Implemented custom directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -9,18 +9,24 @@ angular.module('myApp.directives', [])
 			elm.text(version);
 	};
 }])
-	.directive('apartmentDetails', function () {
+	.directive('apartmentData', function () {
 	    return {
-	        restrict: 'EA', //E = element, A = attribute, C = class, M = comment         
+	        restrict: 'E', //E = element, A = attribute, C = class, M = comment      
+	        replace: true,   
+	        template: "<div class='apartment-data'><h3>Apartments List</h3><ul></ul></div>", //templateUrl: '../templates/apartment_details_template.html',
 	        scope: {
 	            //@ reads the attribute value, = provides two-way binding, & works with functions
-	            title: '@'
+	            apartments: '=apartments'
 	        },
-	        template: "<div class='apartment-details'><h3>Apartment: {{ apartments }}</h3></div>",
-	        //templateUrl: '../templates/apartment_details_template.html',
 
-	        //controller: controllerFunction, //Embed a custom controller in the directive
+	        // controller: controllerFunction, // Embed a custom controller in the directive
 	        link: function ($scope, element, attrs) { 
+
+	        	$scope.$watch('apartments', function(apartments) {
+	        		angular.forEach(apartments, function(apartment, key) {
+	        			element.find("ul").append('<li><a href="#">' + apartment.Address + "</a></li>");
+	        		});
+	        	});
 
 	        } //DOM manipulation
 	    };
