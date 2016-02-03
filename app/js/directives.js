07fd1a4a255edce126ffb243753db0937a474565
justinkjchang <justin.kj.chang@gmail.com>
2016-02-03 05:28:10 -0500

    Moved template to it's own file

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,8 +1,6 @@
 'use strict';
 
 /* Directives */
-
-
 angular.module('myApp.directives', [])
 	.directive('appVersion', ['version', function(version) {
 		return function(scope, elm, attrs) {
@@ -11,23 +9,22 @@ angular.module('myApp.directives', [])
 }])
 	.directive('apartmentData', function () {
 	    return {
-	        restrict: 'E', //E = element, A = attribute, C = class, M = comment      
+	        restrict: 'E', // E = element, A = attribute, C = class, M = comment      
 	        replace: true,   
-	        template: "<div class='apartment-data'><h3>Apartments List</h3><ul></ul></div>", //templateUrl: '../templates/apartment_details_template.html',
+	        templateUrl: 'templates/apartment_data_template.html',
 	        scope: {
-	            //@ reads the attribute value, = provides two-way binding, & works with functions
+	            // @ reads the attribute value, = provides two-way binding, & works with functions
 	            apartments: '=apartments'
 	        },
 
 	        // controller: controllerFunction, // Embed a custom controller in the directive
 	        link: function ($scope, element, attrs) { 
-
+	        	// DOM manipulation
 	        	$scope.$watch('apartments', function(apartments) {
 	        		angular.forEach(apartments, function(apartment, key) {
 	        			element.find("ul").append('<li><a href="#">' + apartment.Address + "</a></li>");
 	        		});
 	        	});
-
-	        } //DOM manipulation
+	        } 
 	    };
 	});
